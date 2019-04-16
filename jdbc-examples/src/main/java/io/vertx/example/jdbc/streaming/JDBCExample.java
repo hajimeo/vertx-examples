1039c852c90be77de3f9f28d4b1711a310b5cccd
hajime <github@hajigle.com>
2019-04-16 14:15:34 +1000

    Using pool and asynchronous

--- jdbc-examples/src/main/java/io/vertx/example/jdbc/streaming/JDBCExample.java
+++ jdbc-examples/src/main/java/io/vertx/example/jdbc/streaming/JDBCExample.java
@@ -7,10 +7,15 @@ import io.vertx.ext.jdbc.JDBCClient;
 import io.vertx.ext.sql.SQLConnection;
 import io.vertx.ext.sql.SQLRowStream;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /*
  * @author <a href="mailto:pmlopes@gmail.com">Paulo Lopes</a>
  */
 public class JDBCExample extends AbstractVerticle {
+  DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
   // Convenience method so you can run it in your IDE
   public static void main(String[] args) {
@@ -23,27 +28,30 @@ public class JDBCExample extends AbstractVerticle {
     final JDBCClient client = JDBCClient.createShared(vertx, new JsonObject()
       .put("url", "jdbc:hive2://atscale731:11111/")
       .put("driver_class", "org.apache.hive.jdbc.HiveDriver")
-      .put("max_pool_size", 5)
+      .put("max_pool_size", 10)
       .put("user", "admin")
       .put("password", "admin"));
 
-    client.getConnection(conn -> {
-      if (conn.failed()) {
-        System.err.println(conn.cause().getMessage());
-        return;
-      }
+    for (int i = 0; i < 40; i++) {
+      String s = Integer.toString(i); //variable used in lambda should be final or effectively final
 
-      // Not asynchronous way but just looping
-      for (int i = 0; i < 40; i++) {
+      client.getConnection(conn -> {
+        if (conn.failed()) {
+          System.err.println("[" + dateFormat.format(new Date()) + "] " + conn.cause().getMessage());
+          return;
+        }
+
+        // Not asynchronous way but just looping
         final SQLConnection connection = conn.result();
         connection.queryStream("SELECT Gender, SUM(orderquantity1) AS q FROM `Sales Insights`.`Internet Sales Cube` GROUP BY Gender", stream -> {
           if (stream.succeeded()) {
+            System.err.println("[" + dateFormat.format(new Date()) + "] stream Success! " + s);
             SQLRowStream sqlRowStream = stream.result();
 
             sqlRowStream
               .handler(row -> {
                 // do something with the row...
-                System.out.println(row.encode());
+                System.out.println(row.toString());
               })
               .endHandler(v -> {
                 // no more data available, close the connection
@@ -53,9 +61,11 @@ public class JDBCExample extends AbstractVerticle {
                   }
                 });
               });
+          } else {
+            System.err.println("[" + dateFormat.format(new Date()) + "] stream Failed! " + s);
           }
         });
-      }
-    });
+      });
+    }
   }
 }
